5cb9aefc16e0430e2a57ec88fb2e7cae2111fb69
Nils Bussas <nils.bussas@gmail.com>
2022-05-25 19:02:01 +0200

    Add Graph.Leaves()

---
+++ pkg/graph/leaves.go
@@ -0,0 +1,55 @@
+package graph
+
+import (
+	"errors"
+)
+
+var ErrNotLeafable = errors.New("cannot build valid leaf graph when nodes aren't fully inherited")
+
+func (g *Graph) Leaves() (*Graph, error) {
+	out := New(nil)
+
+	mapping := make(map[NodeIndex]NodeIndex)
+	g.leaves(NodeIndex{}, out, mapping)
+
+	return out, g.leafEdges(out, mapping)
+}
+
+func (g *Graph) leaves(nidx NodeIndex, out *Graph, mapping map[NodeIndex]NodeIndex) {
+	node := g.Node(nidx)
+	children := g.Children(nidx)
+	if len(children) == 0 {
+		cidx, _ := out.Add(NodeIndex{}, nil)
+		outNode := out.Node(cidx)
+		for k, v := range node.Properties {
+			outNode.Properties[k] = v
+		}
+		mapping[nidx] = cidx
+	} else {
+		for _, cidx := range children {
+			g.leaves(cidx, out, mapping)
+		}
+	}
+}
+
+func (g *Graph) leafEdges(out *Graph, mapping map[NodeIndex]NodeIndex) error {
+	for eidx := EdgeIndex(0); int(eidx) < g.countEdges(); eidx++ {
+		edgeNodes := g.Nodes(eidx)
+
+		var oidxs [2]NodeIndex
+		for j := 0; j < 2; j++ {
+			if oidx, ok := mapping[edgeNodes[j][len(edgeNodes[j])-1]]; ok {
+				oidxs[j] = oidx
+			} else {
+				return ErrNotLeafable
+			}
+		}
+
+		oeidx, _ := out.Link(oidxs[0], oidxs[1])
+		new := out.Edge(oeidx)
+		for k, v := range g.Edge(eidx).Properties {
+			new.Properties[k] = v
+		}
+	}
+	return nil
+}
---
+++ pkg/graph/leaves_test.go
@@ -0,0 +1,131 @@
+package graph_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/nilsbu/arch/pkg/graph"
+	tg "github.com/nilsbu/arch/test/graph"
+)
+
+func TestLeaves(t *testing.T) {
+	for _, c := range []struct {
+		name   string
+		graph  func() *graph.Graph
+		expect func() *graph.Graph
+		err    error
+	}{
+		{
+			"root only",
+			func() *graph.Graph { return graph.New(nil) },
+			func() *graph.Graph {
+				g := graph.New(nil)
+				g.Add(graph.NodeIndex{}, nil)
+				return g
+			},
+			nil,
+		},
+		{
+			"single child with property",
+			func() *graph.Graph {
+				g := graph.New(nil)
+				nidx, _ := g.Add(graph.NodeIndex{}, nil)
+				g.Node(nidx).Properties["a"] = "b"
+				return g
+			},
+			func() *graph.Graph {
+				g := graph.New(nil)
+				nidx, _ := g.Add(graph.NodeIndex{}, nil)
+				g.Node(nidx).Properties["a"] = "b"
+				return g
+			},
+			nil,
+		},
+		{
+			"two linked nodes",
+			func() *graph.Graph {
+				g := graph.New(nil)
+				n0, _ := g.Add(graph.NodeIndex{}, nil)
+				n1, _ := g.Add(graph.NodeIndex{}, nil)
+				eidx, _ := g.Link(n0, n1)
+				g.Edge(eidx).Properties["a"] = "b"
+				return g
+			},
+			func() *graph.Graph {
+				g := graph.New(nil)
+				n0, _ := g.Add(graph.NodeIndex{}, nil)
+				n1, _ := g.Add(graph.NodeIndex{}, nil)
+				eidx, _ := g.Link(n0, n1)
+				g.Edge(eidx).Properties["a"] = "b"
+				return g
+			},
+			nil,
+		},
+		{
+			"not fully inherited edge",
+			func() *graph.Graph {
+				g := graph.New(nil)
+				n10, _ := g.Add(graph.NodeIndex{}, nil)
+				n11, _ := g.Add(graph.NodeIndex{}, nil)
+				e0, _ := g.Link(n10, n11)
+				n20, _ := g.Add(n10, nil)
+				n21, _ := g.Add(n10, []graph.EdgeIndex{e0})
+				g.Link(n20, n21)
+				g.Add(n21, nil)
+				return g
+			},
+			func() *graph.Graph {
+				return nil
+			},
+			graph.ErrNotLeafable,
+		},
+		{
+			"node gets inherited",
+			func() *graph.Graph {
+				g := graph.New(nil)
+				n10, _ := g.Add(graph.NodeIndex{}, nil)
+				n11, _ := g.Add(graph.NodeIndex{}, nil)
+				e0, _ := g.Link(n10, n11)
+				g.Edge(e0).Properties["a"] = "b"
+				n20, _ := g.Add(n10, nil)
+				g.Node(n20).Properties["n"] = 0
+				n21, _ := g.Add(n10, []graph.EdgeIndex{e0})
+				g.Node(n21).Properties["n"] = 1
+				e1, _ := g.Link(n20, n21)
+				g.Edge(e1).Properties["a"] = "c"
+				n22, _ := g.Add(n11, []graph.EdgeIndex{e0})
+				g.Node(n22).Properties["n"] = 2
+				return g
+			},
+			func() *graph.Graph {
+				g := graph.New(nil)
+				n0, _ := g.Add(graph.NodeIndex{}, nil)
+				n1, _ := g.Add(graph.NodeIndex{}, nil)
+				n2, _ := g.Add(graph.NodeIndex{}, nil)
+				g.Node(n0).Properties["n"] = 0
+				g.Node(n1).Properties["n"] = 1
+				g.Node(n2).Properties["n"] = 2
+				e0, _ := g.Link(n1, n2)
+				g.Edge(e0).Properties["a"] = "b"
+				e1, _ := g.Link(n0, n1)
+				g.Edge(e1).Properties["a"] = "c"
+				return g
+			},
+			nil,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if leaves, err := c.graph().Leaves(); err != nil && c.err == nil {
+				t.Errorf("unexpected error: %v", err)
+			} else if err == nil && c.err != nil {
+				t.Errorf("expected error but non ocurred")
+			} else if !errors.Is(err, c.err) {
+				t.Errorf("wrong type of error\nexpect: %v\nactual: %v", c.err, err)
+			} else if err == nil {
+				if eq, explain := tg.AreEqual(c.expect(), leaves); !eq {
+					t.Errorf("graphs don't match: %v", explain)
+				}
+			}
+		})
+	}
+}
