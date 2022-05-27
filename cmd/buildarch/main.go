66cce50c70c368db1c36c937ec2786b414ff7004
Nils Bussas <nils.bussas@gmail.com>
2022-05-27 16:40:32 +0200

    Add main, std_rules & first script

---
+++ cmd/buildarch/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nilsbu/arch/pkg/area"
+	"github.com/nilsbu/arch/pkg/blueprint"
+	"github.com/nilsbu/arch/pkg/csp"
+	"github.com/nilsbu/arch/pkg/graph"
+	"github.com/nilsbu/arch/pkg/merge"
+	"github.com/nilsbu/arch/pkg/render"
+	"github.com/nilsbu/arch/pkg/rule"
+	"github.com/nilsbu/arch/pkg/world"
+)
+
+func main() {
+	bps := make([]blueprint.Blueprint, len(os.Args)-1)
+	for i := range bps {
+		if file, err := os.ReadFile(os.Args[i+1]); err != nil {
+			fmt.Println(err)
+			return
+		} else if bps[i], err = blueprint.Parse(file); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	resolver := &merge.Resolver{
+		Name: "@rule",
+		Keys: map[string]rule.Rule{
+			"House":    rule.House{},
+			"Corridor": rule.Corridor{},
+			"TwoRooms": rule.TwoRooms{},
+			"Room":     rule.Room{},
+			"NOP":      rule.NOP{},
+		},
+	}
+
+	if g, err := merge.Build(bps, &csp.Centipede{}, resolver); err != nil {
+		fmt.Println(err)
+	} else {
+		root := (*area.AreaNode)(g.Node(graph.NodeIndex{}))
+		rect := root.GetRect()
+		tiles := world.CreateTiles(rect.X1+1, rect.Y1+1, world.Tile{})
+		if err := draw(g, graph.NodeIndex{}, tiles); err != nil {
+			fmt.Println(err)
+		} else {
+			render.Terminal(os.Stdout, tiles)
+		}
+	}
+}
+
+func draw(g *graph.Graph, nidx graph.NodeIndex, tiles world.Tiles) error {
+	// TODO this is ugly, right a correct function for this
+	a := (*area.AreaNode)(g.Node(nidx))
+	rect := a.GetRect()
+	if rect.X1 == 0 || rect.Y1 == 0 {
+		return fmt.Errorf("rect for %v not set", nidx)
+	} else if render, ok := a.Properties["render-walls"]; !ok || render.(bool) {
+		for x := rect.X0; x <= rect.X1; x++ {
+			tiles.Set(x, rect.Y0, world.Tile{Type: world.Wall})
+			tiles.Set(x, rect.Y1, world.Tile{Type: world.Wall})
+		}
+		for y := rect.Y0; y <= rect.Y1; y++ {
+			tiles.Set(rect.X0, y, world.Tile{Type: world.Wall})
+			tiles.Set(rect.X1, y, world.Tile{Type: world.Wall})
+		}
+	}
+
+	for _, cnidx := range g.Children(nidx) {
+		if err := draw(g, cnidx, tiles); err != nil {
+			return err
+		}
+	}
+
+	// TODO If this is moved before the recursive call, it doesn't work. Why?
+	for _, eidx := range a.Edges {
+		pos := (*area.DoorEdge)(g.Edge(eidx)).GetPos()
+		fmt.Println(pos)
+		tiles.Set(pos.X, pos.Y, world.Tile{Type: world.Free})
+	}
+	return nil
+}
---
+++ pkg/rule/std_rules.go
@@ -0,0 +1,159 @@
+package rule
+
+import (
+	"encoding/json"
+
+	"github.com/nilsbu/arch/pkg/area"
+	"github.com/nilsbu/arch/pkg/blueprint"
+	"github.com/nilsbu/arch/pkg/graph"
+)
+
+type House struct{}
+
+func (r House) ChildParams() []string {
+	return []string{"interior", "exterior"}
+}
+
+func (r House) PrepareGraph(
+	g *graph.Graph,
+	nidx graph.NodeIndex,
+	children map[string][]graph.NodeIndex,
+	bp blueprint.Blueprint,
+) error {
+	a := (*area.AreaNode)(g.Node(nidx))
+	a.Properties["render-walls"] = false
+	data := []int{}
+	if err := json.Unmarshal([]byte(bp.Values("rect")[0]), &data); err != nil {
+		return err
+	} else {
+		// Add one to have room at the bottom for the exterior
+		data[3]++
+		a.SetRect(area.Rectangle{X0: data[0], Y0: data[1], X1: data[2], Y1: data[3]})
+
+		chi := make([]*area.AreaNode, 2)
+		chi[0] = (*area.AreaNode)(g.Node(children["interior"][0]))
+		chi[1] = (*area.AreaNode)(g.Node(children["exterior"][0]))
+
+		h := float64(data[3] - data[1])
+		if err := area.Split(a, chi, []float64{(h - 1) / h}, area.Down); err != nil {
+			return err
+		} else {
+			return area.CreateDoor(g, children["interior"][0], children["exterior"][0], .5)
+		}
+	}
+}
+
+type Corridor struct{}
+
+func (r Corridor) ChildParams() []string {
+	return []string{"left", "corridor", "right"}
+}
+
+func (r Corridor) PrepareGraph(
+	g *graph.Graph,
+	nidx graph.NodeIndex,
+	children map[string][]graph.NodeIndex,
+	bp blueprint.Blueprint,
+) error {
+	a := (*area.AreaNode)(g.Node(nidx))
+	rect := a.GetRect()
+
+	chi := make([]*area.AreaNode, 3)
+	chi[0] = (*area.AreaNode)(g.Node(children["left"][0]))
+	chi[1] = (*area.AreaNode)(g.Node(children["corridor"][0]))
+	chi[2] = (*area.AreaNode)(g.Node(children["right"][0]))
+
+	roomOrientation := area.Turn(area.GetDirection(g, nidx, a.Edges[0]), -90)
+	var roomWidth int
+	if roomOrientation == area.Up || roomOrientation == area.Down {
+		roomWidth = rect.X1 - rect.X0 + 1
+	} else {
+		roomWidth = rect.Y1 - rect.Y0 + 1
+	}
+
+	corridorWidth := 3.
+	at := []float64{
+		.5 - corridorWidth/float64(roomWidth)/2,
+		.5 + (corridorWidth+2)/float64(roomWidth)/2,
+	}
+
+	if err := area.Split(a, chi, at, roomOrientation); err != nil {
+		return err
+	} else {
+		for _, side := range []string{"left", "right"} {
+			cdren := make([]*area.AreaNode, len(children[side]))
+			for i, cnidx := range children[side] {
+				cdren[i] = (*area.AreaNode)(g.Node(cnidx))
+			}
+			at := make([]float64, len(children[side])-1)
+			for i := range at {
+				at[i] = float64(i+1) / float64(len(children[side]))
+			}
+			if err := area.Split((*area.AreaNode)(g.Node(children[side][0])), cdren, at, area.Turn(roomOrientation, 90)); err != nil {
+				return err
+			}
+			for _, cnidx := range children[side] {
+				if err := area.CreateDoor(g, children["corridor"][0], cnidx, .5); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}
+}
+
+type TwoRooms struct{}
+
+func (r TwoRooms) ChildParams() []string {
+	return []string{"rooms"}
+}
+
+func (r TwoRooms) PrepareGraph(
+	g *graph.Graph,
+	nidx graph.NodeIndex,
+	children map[string][]graph.NodeIndex,
+	bp blueprint.Blueprint,
+) error {
+	a := (*area.AreaNode)(g.Node(nidx))
+	chi := make([]*area.AreaNode, len(children["rooms"]))
+	for i, nidx := range children["rooms"] {
+		chi[i] = (*area.AreaNode)(g.Node(nidx))
+	}
+	roomOrientation := area.Turn(area.GetDirection(g, nidx, a.Edges[0]), 180)
+	if err := area.Split(a, chi, []float64{.5}, roomOrientation); err != nil {
+		return err
+	} else {
+		return area.CreateDoor(g, children["rooms"][0], children["rooms"][1], .5)
+	}
+}
+
+type Room struct{}
+
+func (r Room) ChildParams() []string {
+	return []string{}
+}
+
+func (r Room) PrepareGraph(
+	g *graph.Graph,
+	nidx graph.NodeIndex,
+	children map[string][]graph.NodeIndex,
+	bp blueprint.Blueprint,
+) error {
+	return nil
+}
+
+type NOP struct{}
+
+func (r NOP) ChildParams() []string {
+	return []string{}
+}
+
+func (r NOP) PrepareGraph(
+	g *graph.Graph,
+	nidx graph.NodeIndex,
+	children map[string][]graph.NodeIndex,
+	bp blueprint.Blueprint,
+) error {
+	g.Node(nidx).Properties["render-walls"] = false
+	return nil
+}
