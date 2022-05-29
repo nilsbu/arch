380956df89e699816dd1f345ff4a283f2688c697
Nils Bussas <nils.bussas@gmail.com>
2022-05-29 17:25:15 +0200

    Use lowest EdgeIndex in RoomOrientation

--- pkg/rule/helpers.go
+++ pkg/rule/helpers.go
@@ -2,6 +2,7 @@ package rule
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/nilsbu/arch/pkg/area"
 	"github.com/nilsbu/arch/pkg/graph"
@@ -10,7 +11,13 @@ import (
 // RoomOrientation identifies the orientation of an area.
 // The area is defined as the direction someone is looking in when they enter through the first door.
 func RoomOrientation(g *graph.Graph, nidx graph.NodeIndex) area.Direction {
-	return area.Turn(area.GetDirection(g, nidx, g.Node(nidx).Edges[0]), 180)
+	minEidx := graph.EdgeIndex(math.MaxInt64)
+	for _, eidx := range g.Node(nidx).Edges {
+		if minEidx > eidx {
+			minEidx = eidx
+		}
+	}
+	return area.Turn(area.GetDirection(g, nidx, minEidx), 180)
 }
 
 // InheritEdges passes on the edges of a parent to children depending on their position.
